bed93a01118a3dfdc5adce61c7207c2177c9dea3
Adam Wienconek <adamwienconek@gmail.com>
2020-11-29 13:43:37 +0100

    Added license info

--- Sources/SPTKitModels/Other/SPTSearchResponse.swift
+++ Sources/SPTKitModels/Other/SPTSearchResponse.swift
@@ -17,41 +17,31 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import Foundation
-
-/// Simplified Playlist object.
-public class SPTSimplifiedArtist: SPTBaseObject {
+/**
+ Container class for encapsuling objects included in search response from Spotify service.
+ */
+public class SPTSearchResponse: Codable {
     /**
-     The name of this artist.
+     Simplified track objects included in the response, wrapped in paging object.
      */
-    public let name: String
+    public let tracks: SPTPagingObject<SPTSimplifiedTrack>?
     
-    public override var description: String {
-        return """
-           Artist: \"\(name)\", uri: \(uri)
-        """
-    }
+    /**
+     Simplified album objects included in the response, wrapped in paging object.
+     */
+    public let albums: SPTPagingObject<SPTSimplifiedAlbum>?
     
-    // MARK: Codable stuff
-    private enum CodingKeys: String, CodingKey {
-        case name
-    }
+    /**
+     Simplified artist objects included in the response, wrapped in paging object.
+     */
+    public let artists: SPTPagingObject<SPTSimplifiedArtist>?
     
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-        try super.init(from: decoder)
-    }
-
-    public override func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-        try super.encode(to: encoder)
-    }
-}
-
-extension SPTSimplifiedArtist: Nestable {
-    public static var pluralKey: String {
-        return "artists"
-    }
+    /**
+     Simplified playlist objects included in the response, wrapped in paging object.
+     */
+    public let playlists: SPTPagingObject<SPTSimplifiedPlaylist>?
+    
+//    public let shows: SPTPagingObject<SPTSimplifiedAlbum>?
+//
+//    public let episodes: SPTPagingObject<SPTSimplifiedAlbum>?
 }
